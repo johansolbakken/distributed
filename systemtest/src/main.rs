0c6ceb50fcf91b7a2fb8ef1294e8007039212456
Johan Solbakken <johsol@stud.ntnu.no>
2024-04-08 11:55:12 +0200

    Systemtest starts the conductor

--- systemtest/src/main.rs
+++ systemtest/src/main.rs
@@ -1,3 +1,26 @@
+// This test script starts the processes that will be used to test the distributed network.
+// Currently, it only starts the conductor process.
+
 fn main() {
-    println!("Hello, world!");
+    let binary_path = std::env::current_exe().expect("failed to get current executable path");
+    let conductor_path = binary_path
+        .parent()
+        .expect("failed to get parent directory")
+        .join("conductor");
+    let _node_path = binary_path
+        .parent()
+        .expect("failed to get parent directory")
+        .join("node");
+
+    let child = std::process::Command::new(conductor_path)
+        .spawn()
+        .expect("failed to start conductor");
+
+    let output = child.wait_with_output().expect("failed to wait on child");
+
+    if output.status.success() {
+        println!("conductor exited successfully");
+    } else {
+        println!("conductor exited with: {}", output.status);
+    }
 }
